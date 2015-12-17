c47d865f43fa7079d8fe3419f351b380d064fb8e
Emil Andersson <vaccuum@gmail.com>
2015-12-17 21:58:41 +0100

    Rename classes to please Maven

--- src/test/java/devember/BaseTestClass.java
+++ src/test/java/devember/BaseTestClass.java
@@ -4,7 +4,7 @@ package devember;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterSuite;
 
-public class TestBase {
+public class BaseTestClass {
     Browser browser = new Browser();
     WebDriver driver = browser.Driver;
     ExamplePage example = new ExamplePage(driver);
--- src/test/java/devember/TestExample.java
+++ src/test/java/devember/TestExample.java
@@ -3,15 +3,15 @@ package devember;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class ExampleTests extends TestBase {
+public class TestExample extends BaseTestClass {
 
     @Test
-    public void simpleExampleTest() {
+    public void simpleExample() {
         Assert.assertTrue(true, "Herp derp");
     }
 
     @Test
-    public void navigateToExampleTest() {
+    public void navigateToExample() {
         example.GoTo();
         Assert.assertTrue(example.isAt(), "Navigated to example.com");
     }
