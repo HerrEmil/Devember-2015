a1a3bbd8b5eb035c43592e520e0f3a5a9fb367a6
Emil Andersson <vaccuum@gmail.com>
2015-12-20 11:59:39 +0100

    Removed first placeholder page object and test
    No longer needed when starting to write real tests

--- src/test/java/devember/BaseTestClass.java
+++ src/test/java/devember/BaseTestClass.java
@@ -7,7 +7,6 @@ import org.testng.annotations.AfterSuite;
 public class BaseTestClass {
     Browser browser = new Browser();
     WebDriver driver = browser.Driver;
-    ExamplePage example = new ExamplePage(driver);
     SF sf = new SF(driver);
 
     @AfterSuite
