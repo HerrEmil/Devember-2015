5e32fd513d60ef64e0aeed39cc6b790a5f7caa7a
Emil Andersson <vaccuum@gmail.com>
2015-12-23 21:49:10 +0100

    Removed unused imports

--- src/main/java/devember/pageobjects/TopMenu.java
+++ src/main/java/devember/pageobjects/TopMenu.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
 public class TopMenu {
     WebDriver driver;
 
--- src/test/java/devember/TestSF.java
+++ src/test/java/devember/TestSF.java
@@ -1,10 +1,6 @@
 package devember;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestSF extends BaseTestClass {
